f8e8ef49ceb2ff97d19fcebadc6a7dae85a9c282
mAmineChniti <emin.chniti@esprit.tn>
2026-02-02 20:31:38 +0100

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,76 +1,64 @@
 import pluginQuery from '@tanstack/eslint-plugin-query';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import reactPlugin from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
 import unicornPlugin from 'eslint-plugin-unicorn';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import tseslint from 'typescript-eslint';
 import parser from '@typescript-eslint/parser';
 import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
 
-const eslintConfig = defineConfig(
-  tseslint.configs.recommendedTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
-  reactHooks.configs.flat.recommended,
-  eslintPluginPrettierRecommended,
-  reactPlugin.configs.flat.recommended,
-  reactPlugin.configs.flat['jsx-runtime'],
+const eslintConfig = defineConfig([
   unicornPlugin.configs.all,
-  [
-    ...nextVitals,
-    globalIgnores([
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-      'postcss.config.mjs',
-    ]),
-    {
-      files: ['**/*.{ts,tsx,js,jsx,mjs,cjs}'],
-      rules: {
-        '@typescript-eslint/array-type': 'off',
-        '@typescript-eslint/consistent-type-definitions': 'off',
-        '@typescript-eslint/no-deprecated': 'warn',
-        '@typescript-eslint/consistent-type-imports': [
-          'warn',
-          { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
-        ],
-        '@typescript-eslint/no-unused-vars': [
-          'warn',
-          { argsIgnorePattern: '^_' },
-        ],
-        '@typescript-eslint/require-await': 'off',
-        '@typescript-eslint/no-misused-promises': [
-          'error',
-          { checksVoidReturn: { attributes: false } },
-        ],
-        'jsx-a11y/alt-text': 'warn',
-        'jsx-a11y/anchor-is-valid': 'warn',
-        'unicorn/no-keyword-prefix': 'off',
-        'unicorn/prevent-abbreviations': 'off',
-      },
-      settings: {
-        react: {
-          version: 'detect',
-        },
+  ...nextVitals,
+  ...nextTs,
+  eslintPluginPrettierRecommended,
+  prettier,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
+  {
+    files: ['**/*.{ts,tsx,js,jsx,mjs,cjs}'],
+    rules: {
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/no-deprecated': 'warn',
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } },
+      ],
+      'jsx-a11y/alt-text': 'warn',
+      'jsx-a11y/anchor-is-valid': 'warn',
+      'unicorn/no-keyword-prefix': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
       },
     },
-    {
-      linterOptions: {
-        reportUnusedDisableDirectives: true,
-      },
-      languageOptions: {
-        parser: parser,
-        parserOptions: {
-          projectService: true,
-          ecmaVersion: 'latest',
-          sourceType: 'module',
-          tsconfigRootDir: import.meta.dirname,
-        },
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    languageOptions: {
+      parser: parser,
+      parserOptions: {
+        projectService: true,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        tsconfigRootDir: import.meta.dirname,
       },
     },
-    ...pluginQuery.configs['flat/recommended'],
-  ]
-);
+  },
+  ...pluginQuery.configs['flat/recommended'],
+]);
 
 export default eslintConfig;
