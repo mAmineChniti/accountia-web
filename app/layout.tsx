0527247fe54f9ecd259c5d24d13d00aee7f01342
mAmineChniti <emin.chniti@esprit.tn>
2026-04-17 12:32:12 +0100

    fixed chatbot

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,9 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Noto_Sans_Arabic } from 'next/font/google';
 import type { ReactNode } from 'react';
 import './globals.css';
 import { Providers } from '@/components/reusable/providers';
 import { TokenExpiration } from '@/components/reusable/token-expiration';
 import { headers } from 'next/headers';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
-
-const notoSansArabic = Noto_Sans_Arabic({
-  variable: '--font-noto-sans-arabic',
-  subsets: ['arabic'],
-  weight: '400',
-});
 
 export const metadata: Metadata = {
   title: 'Accountia',
@@ -42,9 +25,7 @@ export default async function RootLayout({
       className="h-full"
       suppressHydrationWarning
     >
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${notoSansArabic.variable} h-full antialiased`}
-      >
+      <body className="h-full font-sans antialiased">
         <Providers>
           <TokenExpiration />
           {children}
