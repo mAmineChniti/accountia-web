52d0c6cddfbcc42929457b3fe24c99d73aaed5b5
mAmineChniti <emin.chniti@esprit.tn>
2026-04-30 20:35:20 +0100

    fixed favicon again

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/favicon.ico" />
+      <meta name="theme-color" content="#ffffff" />
+    </>
+  );
+}
