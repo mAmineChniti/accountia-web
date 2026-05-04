22819325f297773fe49e522564e59c2967df9c5b
mAmineChniti <emin.chniti@esprit.tn>
2026-05-04 17:44:45 +0100

    fixed a few conf tsx files

--- app/[lang]/(protected)/business/[businessId]/members/page.tsx
+++ app/[lang]/(protected)/business/[businessId]/members/page.tsx
@@ -1,11 +1,13 @@
 import { type Locale } from '@/i18n-config';
 import { BusinessMembers } from '@/components/app/business-members/BusinessMembers';
+import { getDictionary } from '@/get-dictionary';
 
 export default async function MembersPage({
   params,
 }: {
   params: Promise<{ lang: Locale; businessId: string }>;
 }) {
-  const { businessId } = await params;
-  return <BusinessMembers businessId={businessId} />;
+  const { businessId, lang } = await params;
+  const dictionary = await getDictionary(lang);
+  return <BusinessMembers businessId={businessId} dictionary={dictionary} />;
 }
